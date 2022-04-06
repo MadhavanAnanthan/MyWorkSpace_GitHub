f648c0abf10674450ad62d3444b49e6ae9cc0a37
A743628 <A743628@192.168.0.197>
2022-04-06 20:19:10 +0530

    commit form office machine

--- Problems/src/com/problem/easy/FirstCode.java
+++ Problems/src/com/problem/easy/FirstCode.java
@@ -3,6 +3,7 @@ package com.problem.easy;
 public class FirstCode {
 
 	public static void main(String[] args) {
+		// Added this comment from office machine
 		System.out.println("First Code");
 
 	}
